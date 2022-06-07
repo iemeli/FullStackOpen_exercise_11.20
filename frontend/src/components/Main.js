e0873e9f05752392449d920b7dab3c98f1c367db
iemeli <eemeli@paalimaki.fi>
2022-06-07 15:55:50 +0300

    Test discord webhook info

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -4,7 +4,7 @@ const Main = () => {
   return (
     <div className="Container">
       <h1>React Node Template</h1>
-      <p>–with mobile responsive navbar, VERSION 2</p>
+      <p>–with mobile responsive navbar, VERSION 3!!!</p>
     </div>
   )
 }
